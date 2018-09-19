448472b638d8babe89dbc2fd0fa918d600f3b1ea
Paolo <paolosbtn@gmail.com>
2018-09-19 20:23:47 +0200

    code giving npix mean for each run

--- root/npix.C
+++ root/npix.C
@@ -1,10 +1,45 @@
 #include<vector>
 #include<map>
 
-void npix(TString filename){
+double npix(TString filename, TString mode){
 
-  std::cout<<filename<<std::endl;
+  TFile* f = new TFile(filename,"R");
+  //  f->ls();
+  TDirectory* dir_dut = (TDirectory*)f->Get("Plane3");
+  /*
+  std::cout<<"plane 3 entries cluster "<<((TTree*)dir_dut->Get("Clusters"))->GetEntries()<<std::endl;
+  std::cout<<"event entries "<<((TTree*)f->Get("Event"))->GetEntries()<<std::endl;
+  */
 
+  Int_t NHits = 0;
+  Int_t inCluster[10];
+  
+  
+  TTree* hits = (TTree*)dir_dut->Get("Hits");
+  //  hits->Print();
 
+  hits->SetBranchAddress("NHits",&NHits);
+  hits->SetBranchAddress("HitInCluster",inCluster);
+  
+  ULong64_t ENTRIES = 50;//tree->GetEntries();
 
+  TH1F* h_npix = new TH1F("h_npix","; Number of pixels in cluster; AU",10,-0.5,9.5);
+  
+  for (int i = 0; i<ENTRIES; i++)
+    {
+      hits->GetEntry(i);
+      Int_t pixInCluster = 0;
+      for (int ipix =0; ipix <NHits; ipix++)
+	{
+
+	  if (inCluster[ipix]!=0) pixInCluster++;
+	}
+      if (pixInCluster!=0) h_npix->Fill(pixInCluster);
+    }
+
+  double output;
+  if (mode.EqualTo("mean")) output = h_npix->GetMean();
+  else output = h_npix->GetMeanError();
+  return output;
+  
 }
--- testbeam.py
+++ testbeam.py
@@ -2,6 +2,7 @@
 
 import os, glob
 import argparse
+import sys,subprocess
 
 sample_dir = "sample"
 
@@ -13,9 +14,14 @@ args = parser.parse_args()
 wildcard = sample_dir+"/*"+args.sample+".root"
 list_samples = glob.glob(wildcard)
 
+dict_output = {}
+
 for f in list_samples:
-    
+    run = f.split("/")[1][0:9]
     if args.npix:
-        cmd = "root -l -q 'root/npix.C("+'"'+str(f)+'"'+")'"
-        print cmd
-        os.system(cmd)
+        cmd = "root -l -q 'root/npix.C("+'"'+str(f)+'"'+","+'"mean"'+")'"
+        dict_output [run+"_mean"] = float(os.popen(cmd).read().replace('(double)','').split("\n")[2])
+        cmd = "root -l -q 'root/npix.C("+'"'+str(f)+'"'+","+'"err"'+")'"
+        dict_output [run+"_meanerr"] = float(os.popen(cmd).read().replace('(double)','').split("\n")[2])
+
+print dict_output
