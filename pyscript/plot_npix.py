5900cafeea3bdfa43e914bd591ff9a6e0c4d145e
Paolo <paolosbtn@gmail.com>
2018-09-24 19:13:19 +0200

    ultimated npix and tot plots vs HV

--- pyscript/plot_npix.py
+++ pyscript/plot_npix.py
@@ -3,7 +3,7 @@
 #import numpy as np
 #import seaborn as sns
 import matplotlib.pyplot as plt
-
+import math
 
 
 def npix_vs_run(dict_output):
@@ -22,3 +22,65 @@ def npix_vs_run(dict_output):
     plt.plot(x,y)
     plt.show()
     
+def npix_vs_hv(dict_output):
+    d_mu = {}
+    d_sigma = {}
+    for name, value in dict_output.iteritems():
+        run = int(name[4:9])
+        if "tot" in name and "mu" in name:
+            d_mu[int(run)] = value
+        if "tot" in name and "sigma" in name:
+            d_sigma[int(run)] = value
+
+    dict_hv = {
+        '50_min': 5580, '50_max': 5620,
+        '80_min': 5630, '80_max': 5670,
+        '100_min': 5680, '100_max': 5720,
+        '150_min': 5780, '150_max': 5820,
+        '200_min': 5830, '200_max': 5870,
+        '300_min': 5880, '300_max': 5920
+    }
+
+    d_mu_avg = {}
+    d_mu_sigavg = {}
+    d_mu_num = {}
+    for label in dict_hv:
+        hv = label.split("_")[0]
+        d_mu_num [hv] = 0
+        d_mu_avg [hv] = 0
+        d_mu_sigavg [hv] = 0
+        
+    for run, value in d_mu.iteritems():
+        for hv_label in dict_hv.keys():
+            hv = hv_label.split("_")[0]
+            if (int(run) <= int(dict_hv[hv+"_max"])) and (int(run) >= int(dict_hv[hv+"_min"])):
+                d_mu_num [hv] += 1
+                d_mu_avg [hv] += float(value)
+
+    for hv,v in d_mu_avg.iteritems():
+        if d_mu_num[hv] == 0:
+            continue
+        d_mu_avg [hv] = float(v) / float(d_mu_num[hv])
+
+
+                
+    for run, value in d_mu.iteritems():
+        for hv_label in dict_hv.keys():
+            hv = hv_label.split("_")[0]
+            if (int(run) <= int(dict_hv[hv+"_max"])) and (int(run) >= int(dict_hv[hv+"_min"])):
+                d_mu_sigavg [hv] += (value-d_mu_avg[hv])*(value-d_mu_avg[hv])/(float(d_mu_num[hv])-1)
+
+    for hv,v in d_mu_sigavg.iteritems():
+        d_mu_sigavg [hv] = math.sqrt(float(v))
+ 
+                
+    l = sorted(d_mu_avg.items())
+    x,y = zip(*l)
+    err_l = sorted(d_mu_sigavg.items())
+    err_x,err_y = zip(*err_l)
+    plt.xlabel("HV [V]")
+    plt.ylabel("Average ToT of cluster on track")
+    plt.errorbar(x,y,err_y,marker='.', mfc='black',ms=10,color="black",ls="")
+    plt.xlim((0,350))
+    plt.ylim((0,8))
+    plt.show()
--- testbeam.py
+++ testbeam.py
@@ -8,6 +8,24 @@ import plot_npix
 import plot_tot
 import plot_eff
 
+def skim_list_samples(tune, list_samples):
+    skim_list = list_samples[:]
+    if tune == "all":
+        return skim_list; 
+    elif tune == "8at16t2p5k":
+        for f in list_samples:
+            n = int(f[10:16])
+            if (n <= int(5932)) or (n >= int(5983)):
+                skim_list.remove(f)
+                
+    elif tune == "8at16t2k":
+        for f in list_samples:
+            n = int(f[10:16])
+            if (n <= int(5376)) or (n >= int(5925)):
+                skim_list.remove(f)
+    else:
+        print ">>> tune %s does not exist: running all" % (tune)
+    return skim_list;
 
 sample_dir = "sample"
 
@@ -15,7 +33,9 @@ parser = argparse.ArgumentParser(description='Launch analysis on testbeam.')
 parser.add_argument('--npix', dest='npix', action = "store_true", default=False, help='Execute the npix analysis: npix')
 parser.add_argument('--tot', dest='tot', action = "store_true", default=False, help='Execute the npix analysis: tot')
 parser.add_argument('--eff',dest='eff', action = "store_true", default=False, help='Execute the npix analysis: eff')
+parser.add_argument('--tune',dest='tune', choices = ['all','8at16t2k','8at16t2p5k'] ,default="all", help='WPs to analyse')
 args = parser.parse_args()
+
 
 sample_type = ""
 if args.npix or args.tot:
@@ -23,13 +43,18 @@ if args.npix or args.tot:
 else:
     sample_type = 'tracks-data'
 
-wildcard = sample_dir+"/*"+sample_type+".root"
+
+wildcard = sample_dir+"/*00580*"+sample_type+".root"
 list_samples = glob.glob(wildcard)
+skim_samples = skim_list_samples(args.tune, list_samples);
 
 dict_output = {}
+counter_file = 1;
 
-for f in list_samples:
+
+for f in skim_samples:
     run = f.split("/")[1][0:9]
+    print "> Processing file: %s (%s/%s) " %(f,counter_file, len(skim_samples))
     if args.npix:
         cmd = "root -l -q 'root/npix.C("+'"'+str(f)+'"'+","+'"mean"'+")'"
         dict_output [run+"_npix_mu"] = float(os.popen(cmd).read().replace('(double)','').split("\n")[2])
@@ -46,14 +71,14 @@ for f in list_samples:
         # dict_output [run+"_eff_mu"] = float(os.popen(cmd).read().replace('(double)','').split("\n")[2])
         # cmd = "root -l -q 'root/tot.C("+'"'+str(f)+'"'+","+'"err"'+")'"
         # dict_output [run+"_eff_sigma"] = float(os.popen(cmd).read().replace('(double)','').split("\n")[2])
-
+    counter_file = counter_file + 1
 
 
 if args.npix:
-    plot_npix.npix_vs_run(dict_output)
+    plot_npix.npix_vs_hv(dict_output)
 
 if args.tot:
-    plot_tot.tot_vs_run(dict_output)
+    plot_tot.tot_vs_hv(dict_output)
 
 # if args.eff:
 #     plot_eff.eff_vs_run(dict_output)
